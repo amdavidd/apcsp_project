3a6bcc9e0ea89ddbe9d8dfd3f59fca43a848372c
amdavidd <david.kang@hkaa.edu.hk>
2025-05-20 07:54:53 +0000

    Imported datasets, cleaned data

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
+genData = pd.read_csv("Dataset/name_gender_dataset.csv")
+genData.drop(['Probability'], axis = 1, inplace = True)
+print(genData.head())
+
+model = LogisticRegression()
+
+train_data = genData['name']
+train_labels = genData['gender']
+
+
+model.fit(train_data, train_labels)
